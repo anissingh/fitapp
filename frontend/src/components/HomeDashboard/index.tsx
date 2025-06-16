13cef4cd6b9abbc2932038b0b8df16afbe3f85b4
Anis Singh <anissingh04@gmail.com>
2025-06-16 01:16:08 -0400

    Bootstrap added

--- frontend/src/components/HomeDashboard/index.tsx
+++ frontend/src/components/HomeDashboard/index.tsx
@@ -1,5 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export const HomeDashboard = () => {
     return (
-        <div>Home Page</div>
-    )
+        <>
+            <div>Home Page</div>
+            <button type="button" className="btn btn-primary">Primary</button>
+        </>
+    );
 }
